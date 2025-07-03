e4a104382030b86da0a17bb7bb058f18d8ac568e
Daniel Villlalobos <daniel.villalobosdel@icloud.com>
2025-07-03 12:07:06 +0200

    Clean start

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  testMatch: ['**/tests/**/*.test.js'],
-  verbose: true,
-  forceExit: true,
-  clearMocks: true,
-  resetMocks: true,
-  restoreMocks: true,
-};
